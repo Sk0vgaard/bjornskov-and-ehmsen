b0046069be7016495afceb9cf4ac2ef036cc10c9
Mathias Skovgaard <sk0vgaard@live.dk>
2023-01-17 20:37:11 +0100

    Add dialog when clicking category image

--- src/app/container/navigation/image-overview/image-overview.component.ts
+++ src/app/container/navigation/image-overview/image-overview.component.ts
@@ -29,35 +29,35 @@ export class ImageOverviewComponent {
   images: Image[] = [
     {
       id: 1,
-      title: 'Image 1',
+      title: 'Gulve',
       url: 'assets/images/120034608_2708637782718165_7918138857527630262_n.jpg',
       description: 'Image 1 Description',
       category: 'floor'
     },
     {
       id: 2,
-      title: 'Image 2',
+      title: 'Vinduer',
       url: 'assets/images/122550057_3537125253001989_7221914740336158703_n.jpg',
       description: 'Image 2 Description',
       category: 'window'
     },
     {
       id: 3,
-      title: 'Image 3',
+      title: 'Døre',
       url: 'https://via.placeholder.com/250x300',
       description: 'Image 3 Description',
       category: 'door'
     },
     {
       id: 4,
-      title: 'Image 4',
+      title: 'Tag',
       url: 'assets/images/311462781_1996719743854758_4004257398874546759_n.jpg',
       description: 'Image 4 Description',
       category: 'roof'
     },
     {
       id: 5,
-      title: 'Image 5',
+      title: 'Udbygning',
       url: 'assets/images/120034608_2708637782718165_7918138857527630262_n.jpg',
       description: 'Image 5 Description',
       category: 'extension'
@@ -69,11 +69,9 @@ export class ImageOverviewComponent {
 
   config: SwiperOptions = {
     slidesPerView: 4,
-    spaceBetween: 50,
+    spaceBetween: 20,
+    autoplay: true,
     navigation: true,
-    fadeEffect: {
-      crossFade: true
-    },
     scrollbar: {draggable: true},
   };
 
