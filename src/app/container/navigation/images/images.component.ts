b41b5c14164bf9299af937a712d8df25f287a98c
Mathias Skovgaard <sk0vgaard@live.dk>
2023-01-17 13:53:30 +0100

    Add image slider with images

--- src/app/container/navigation/images/images.component.ts
+++ src/app/container/navigation/images/images.component.ts
@@ -1,18 +1,41 @@
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SwiperModule } from 'swiper/angular';
+import SwiperCore, { Navigation, Pagination, Scrollbar, A11y, SwiperOptions } from 'swiper';
+
+SwiperCore.use([Navigation, Pagination, Scrollbar, A11y]);
+
 
 @Component({
   selector: 'app-images',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, SwiperModule],
   templateUrl: './images.component.html',
   styleUrls: ['./images.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  encapsulation: ViewEncapsulation.None
 })
 export class ImagesComponent implements OnInit {
 
   ngOnInit(): void {
     console.log('test')
   }
+
+  config: SwiperOptions = {
+    slidesPerView: 1,
+    spaceBetween: 50,
+    navigation: true,
+    pagination: { clickable: true },
+    scrollbar: { draggable: true },
+  };
+
+  // // @ts-ignore
+  // public onSwiper([swiper]) {
+  //   console.log(swiper);
+  // }
+  //
+  // public onSlideChange() {
+  //   console.log('slide change');
+  // }
 
 }
