7652a4ab1b424d9312838fa4ee4329926b09280a
Mathias Skovgaard <sk0vgaard@live.dk>
2023-01-18 22:09:19 +0100

    - make footer responsive to mobile - remove fill from svg files to be able to use custom color

--- src/app/container/footer/footer.component.ts
+++ src/app/container/footer/footer.component.ts
@@ -5,11 +5,12 @@ import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 import { MatListModule } from '@angular/material/list';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
+import { MatLineModule } from '@angular/material/core';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [CommonModule, MatIconModule, MatListModule, MatToolbarModule, MatButtonModule],
+  imports: [CommonModule, MatIconModule, MatListModule, MatToolbarModule, MatButtonModule, MatLineModule],
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
@@ -20,16 +21,19 @@ export class FooterComponent {
   cvr: string = '41701048';
   firmName: string = 'Bjørnskov & Ehmsen tømrer og snedker';
   address: string = 'Hededammen 9, 6705 Esbjerg Ø';
+  facebookUrl: string = '';
+  instagramUrl: string = '';
+  linkedinUrl: string = '';
 
-  private iconPath: string = "../../assets/icons";
+  private iconPath: string = '../../assets/icons';
 
   constructor(
     private matIconRegistry: MatIconRegistry,
     private domSanitizer: DomSanitizer
   ) {
     this.matIconRegistry
-      .addSvgIcon("linkedin", this.setPath(`${(this.iconPath)}/linkedin.svg`))
-      .addSvgIcon("instagram", this.setPath(`${(this.iconPath)}/instagram.svg`));
+      .addSvgIcon('linkedin', this.setPath(`${(this.iconPath)}/linkedin.svg`))
+      .addSvgIcon('instagram', this.setPath(`${(this.iconPath)}/instagram.svg`));
   }
 
   private setPath(url: string): SafeResourceUrl {
@@ -46,5 +50,17 @@ export class FooterComponent {
 
   navigateToAddress() {
     window.open(`https://www.google.com/maps/search/${encodeURI(this.address)}`);
+  }
+
+  openFacebook() {
+    window.open(this.facebookUrl, '_blank');
+  }
+
+  openInstagram() {
+    window.open(this.instagramUrl, '_blank');
+  }
+
+  openLinkedIn() {
+    window.open(this.linkedinUrl, '_blank');
   }
 }
